b8ce8b8b74fc9af217e554fd8cb2841bdf2a1935
Wylliam Rodrigues Dias <wylliamdiasr@gmail.com>
2020-04-22 03:19:27 -0300

    Remoção do "console.log" usado no debug

--- src/index.js
+++ src/index.js
@@ -7,23 +7,13 @@ const io = require('socket.io')(server)
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-
-})
-
 io.on('connection', socket => {
-
   socket.on('sendMsg', data => {
-
-    console.log(data)
-
     socket.broadcast.emit('newMsg', {
       text: data.text,
       id: data.id
     })
   })
-
-  console.log(socket.id)
 })
 
 server.listen(8000)
